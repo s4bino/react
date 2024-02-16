25fa75fc6ca89c2f314aa1693d58528b1a2689c8
Heitor <sabino.heitor@hotmail.com>
2024-02-16 00:02:32 -0300

    Aula 17 - roteamento com react-router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './index';
-import Employee from './components/Employee';
-import { useState } from 'react';
-import {v4 as uuidv4} from 'uuid';
-import AddEmployee from './components/AddEmployee';
-import EditEmployee from './components/EditEmployee';
 import Header from './components/Header';
 import Employees from './pages/Employees';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
 
   return (
-    <Header>
-      <Employees />
-    </Header>
+    <BrowserRouter>  
+      <Header>
+        <Routes>
+          <Route path='/Employees' element={<Employees />}/>
+        </Routes>
+      </Header>
+    </BrowserRouter>
   );
 }
 
