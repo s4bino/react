4eed7ed33e4a3ffdefa0aafcfe68747a94fb4db3
Heitor <sabino.heitor@hotmail.com>
2024-02-17 00:29:38 -0300

    Aula 21 - Avançando nos estudos sobre useEffect

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -9,6 +9,10 @@ export default function Dictionary(){
     }); //é possivel limitar para qual estado o useEffect liga, utilizando o dependency array
     // que é o segundo parametro que pode ser passado na função useEffect
 
+    // se passarmos nada no dependency array --> ele vai dar update para cada mudança de estado
+    // se passarmos um dependency array vazio --> executará uma vez --> interessante em casos que precisamos fazer algo assim que a página for carregada
+    // se passarmos algo --> ele se importará somnete com o estado do que passamos
+
     return (
         <>
             <input type='text' 
